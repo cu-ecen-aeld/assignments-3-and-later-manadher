84a71be716dc24dfcb0714b4787a659d243234db
manadher <manadher@gmail.com>
2024-09-09 17:25:54 -0700

    Assignment 4-1

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -4,21 +4,31 @@
 #include <stdio.h>
 
 // Optional: use these functions to add debug or error prints to your application
-#define DEBUG_LOG(msg,...)
-//#define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
-#define ERROR_LOG(msg,...) printf("threading ERROR: " msg "\n" , ##__VA_ARGS__)
+#define DEBUG_LOG(msg, ...)
+// #define DEBUG_LOG(msg,...) printf("threading: " msg "\n" , ##__VA_ARGS__)
+#define ERROR_LOG(msg, ...) printf("threading ERROR: " msg "\n", ##__VA_ARGS__)
 
-void* threadfunc(void* thread_param)
+void *threadfunc(void *thread_param)
 {
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+
+    struct thread_data *thread_func_args = (struct thread_data *)thread_param;
+    struct timespec ts_obt;
+    ts_obt.tv_sec = thread_func_args->wait_to_obtain_ms / 1000;
+    ts_obt.tv_nsec = (thread_func_args->wait_to_obtain_ms % 1000) * 1000;
+    struct timespec ts_rel;
+    ts_rel.tv_sec = thread_func_args->wait_to_release_ms / 1000;
+    ts_rel.tv_nsec = (thread_func_args->wait_to_release_ms % 1000) * 1000;
+    nanosleep(&ts_obt, NULL);
+    pthread_mutex_lock(thread_func_args->mutex);
+    nanosleep(&ts_rel, NULL);
+    pthread_mutex_unlock(thread_func_args->mutex);
     return thread_param;
 }
 
-
-bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int wait_to_obtain_ms, int wait_to_release_ms)
+bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex, int wait_to_obtain_ms, int wait_to_release_ms)
 {
     /**
      * TODO: allocate memory for thread_data, setup mutex and wait arguments, pass thread_data to created thread
@@ -28,6 +38,14 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
+    struct thread_data *thread_func_args = (struct thread_data *)malloc(sizeof(struct thread_data));
+    if (!thread_func_args)
+        return false;
+    thread_func_args->wait_to_obtain_ms = wait_to_obtain_ms;
+    thread_func_args->wait_to_release_ms = wait_to_release_ms;
+    thread_func_args->mutex = mutex;
+    if (pthread_create(thread, NULL, threadfunc, thread_func_args))
+        return false;
+    thread_func_args->thread_complete_success = true;
+    return true;
 }
-
