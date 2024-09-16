8af91ae652ee43f4ccaff6cda5a886fb1c71b62e
manadher <manadher@gmail.com>
2024-09-16 14:35:00 -0700

    Absolute path to /conf/

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -33,7 +33,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=`cat conf/assignment.txt`
+assignment=`cat $FINDER_APP_DIR/conf/assignment.txt`
 
 if [ $assignment != 'assignment1' ]
 then
